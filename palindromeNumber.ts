fef1a6cdccc549d3044a6a3420a8e65bf94b8051
Chase Mortensen <chase@chase-mortensen.dev>
2023-08-18 20:10:23 -0600

    3.1 Add non-string version of isPalindrome

--- palindromeNumber.ts
+++ palindromeNumber.ts
@@ -1,4 +1,25 @@
 function isPalindrome(x: number): boolean {
+    // without converting to a string
+    if (x < 0) {
+        return false
+    }
+    let divisor = 1
+    while (x / divisor >= 10) {
+        divisor *= 10
+    }
+    while (x !== 0) {
+        let front = Math.floor(x / divisor)
+        let back = x % 10
+        if (front !== back) {
+            return false
+        }
+        x = Math.floor((x % divisor) / 10)
+        divisor /= 100
+    }
+    return true
+};
+
+function isPalindromeStringVersion (x: number) {
     // convert to a string
     const xStr = String(x)
     let front = 0, back = xStr.length - 1
@@ -12,14 +33,18 @@ function isPalindrome(x: number): boolean {
     }
     console.log(xStr + ' is a palindrome')
     return true
+}
 
-    // without converting to a string
-    // if (x >= 0) {
+function printPalindrome(x: number) {
+    if (isPalindrome(x)) { // or isPalindromeStringVersion
+        console.log(x + ' is a palindrome')
+    }
+    else {
+        console.log(x + ' is not a palindrome')
+    }
+}
 
-    // }
-    // return false
-};
-
-isPalindrome(121)
-isPalindrome(-121)
-isPalindrome(10)
+printPalindrome(121)
+printPalindrome(1234321)
+printPalindrome(-121)
+printPalindrome(10)
